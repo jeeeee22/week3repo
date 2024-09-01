1808814b3624676cf4a59d64898edc7685f91c64
jeeeee22 <108650409+jeeeee22@users.noreply.github.com>
2024-09-01 20:33:01 +1200

    Task #3

--- Program.cs
+++ Program.cs
@@ -1,106 +1,74 @@
-﻿using EntityModels.Models;
+﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using Week3EntityFramework.Dtos;
+using EntityModels.Models;
 
-var context = new IndustryConnectWeek2Context();
-
-//var customer = new Customer
-//{
-//    DateOfBirth = DateTime.Now.AddYears(-20)
-//};
-
-
-//Console.WriteLine("Please enter the customer firstname?");
-
-//customer.FirstName = Console.ReadLine();
-
-//Console.WriteLine("Please enter the customer lastname?");
-
-//customer.LastName = Console.ReadLine();
-
-
-//var customers = context.Customers.ToList();
-
-//foreach (Customer c in customers)
-//{   
-//    Console.WriteLine("Hello I'm " + c.FirstName);
-//}
-
-//Console.WriteLine($"Your new customer is {customer.FirstName} {customer.LastName}");
-
-//Console.WriteLine("Do you want to save this customer to the database?");
-
-//var response = Console.ReadLine();
-
-//if (response?.ToLower() == "y")
-//{
-//    context.Customers.Add(customer);
-//    context.SaveChanges();
-//}
-
-
-
-var sales = context.Sales.Include(c => c.Customer)
-    .Include(p => p.Product).ToList();
-
-var salesDto = new List<SaleDto>();
-
-foreach (Sale s in sales)
+namespace Week3EntityFramework
 {
-    salesDto.Add(new SaleDto(s));
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new IndustryConnectWeek2Context())
+            {
+                context.Database.EnsureCreated();
+                var customersWithoutSales = context.Customers
+                    .Where(c => !c.Sales.Any())
+                    .ToList();
+
+                Console.WriteLine("Customers without sales:");
+                foreach (var customer in customersWithoutSales)
+                {
+                    Console.WriteLine($"Customer: {customer.FirstName} {customer.LastName}");
+                }
+                var newCustomer = new Customer
+                {
+                    FirstName = "New",
+                    LastName = "Customer",
+                    DateOfBirth = new DateTime(1985, 5, 23)
+                };
+                context.Customers.Add(newCustomer);
+                context.SaveChanges();
+
+                var newProduct = context.Products.FirstOrDefault();
+                var newStore = context.Stores.FirstOrDefault();
+
+                if (newProduct != null && newStore != null)
+                {
+                    var newSale = new Sale
+                    {
+                        CustomerId = newCustomer.Id,
+                        ProductId = newProduct.Id,
+                        StoreId = newStore.Id,
+                        DateSold = DateTime.Now
+                    };
+                    context.Sales.Add(newSale);
+                    context.SaveChanges();
+                }
+
+                Console.WriteLine($"Added new customer: {newCustomer.FirstName} {newCustomer.LastName} with a sale record.");
+
+                var anotherStore = new Store
+                {
+                    Name = "New Store"
+                };
+                context.Stores.Add(anotherStore);
+                context.SaveChanges();
+
+                Console.WriteLine($"Added new store: {anotherStore.Name}");
+
+                var storesWithSales = context.Stores
+                    .Where(s => s.Sales.Any())
+                    .ToList();
+
+                Console.WriteLine("Stores with sales:");
+                foreach (var store in storesWithSales)
+                {
+                    Console.WriteLine($"Store: {store.Name}");
+                }
+
+                Console.WriteLine("Operations completed successfully.");
+            }
+        }
+    }
 }
-
-
-
-//context.Sales.Add(new Sale
-//{
-//    ProductId = 1,
-//    CustomerId = 1,
-//    StoreId = 1,
-//    DateSold = DateTime.Now
-//});
-
-
-//context.SaveChanges();
-
-
-
-
-Console.WriteLine("Which customer record would you like to update?");
-
-var response = Convert.ToInt32(Console.ReadLine());
-
-var customer = context.Customers.Include(s => s.Sales)
-    .ThenInclude(p => p.Product)
-    .FirstOrDefault(c => c.Id == response);
-
-
-var total = customer.Sales.Select(s => s.Product.Price).Sum();
-
-
-var customerSales = context.CustomerSales.ToList();
-
-//var totalsales = customer.Sales
-
-
-
-//Console.WriteLine($"The customer you have retrieved is {customer?.FirstName} {customer?.LastName}");
-
-//Console.WriteLine($"Would you like to updated the firstname? y/n");
-
-//var updateResponse = Console.ReadLine();
-
-//if (updateResponse?.ToLower() == "y")
-//{
-
-//    Console.WriteLine($"Please enter the new name");
-
-//    customer.FirstName = Console.ReadLine();
-//    context.Customers.Add(customer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-//    context.SaveChanges();
-//}
-
-
-
-Console.ReadLine();
-
